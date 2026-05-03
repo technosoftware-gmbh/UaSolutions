64d6a67c8b796cab05f7bbb96bbd7489eb0f1d0a
Thomas Johannhson <support@technosoftware.com>
2026-05-03 15:39:22 +0200

    Added nuget release

--- src/Technosoftware/ClientGateway/Common/BuildDesign.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-setlocal
-
-SET PATH=..\..\scripts;..\..\bin\net471;..\..\..\bin\net471;%PATH%;
-
-echo Building ModelDesign
-Technosoftware.UaModelCompiler.exe -d2 ".\ModelDesign.xml" -c ".\ModelDesign.csv" -o ".\"
-echo Success!
